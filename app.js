00d52fd5d1bb92dd02f2cbd42b36bc5ad39faa7d
Bekbolot <bekabazarkulov265@gmail.com>
2025-12-14 21:53:39 +0600

    init

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 class Button {
     constructor(text, type, size) {
         this.text = text;
-        this.type = type;
+        
         this.size = size;
+
+        this.type = type;
     }
 
     render() {
         const button = document.createElement("button");
 
         button.classList.add("button");
-        button.classList.add(this.type); 
+        
         button.classList.add(this.size); 
+        button.classList.add(this.type); 
 
         button.textContent = this.text;
+
 
         document.body.appendChild(button);
     }
 }
 
+new Button("Купить", "primary", "large").render();
+new Button("Отмена", "secondary", "medium").render();
+new Button("Удалить", "danger", "small").render();
